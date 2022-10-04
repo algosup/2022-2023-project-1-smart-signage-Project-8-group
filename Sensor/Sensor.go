09544087745b7091d18fc1eabbe367d50e2387b5
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-10-04 13:40:05 +0200

    added blink led and turn on

--- Sensor/Sensor.go
+++ Sensor/Sensor.go
@@ -1,21 +1,48 @@
 package main
 
-func main() {
+import (
+	"machine"
+	"time"
+)
 
+const led = machine.PA5 //test LED
+const pa4 = machine.PA4 //LED band
+const pb6 = machine.PB6 //High voltage sensor
+const pb7 = machine.PB7 //Low Voltage sensor
+
+func main() {
+	// test if flash is succesfull
+	blinkLED()
 }
 
+//turn on/of LED band
 func powerLED() {
-	//turn on LED
+	if pa4.Get() {
+		pa4.Low()
+	} else {
+		pa4.High()
+	}
 }
 
 func checkPowerStatusH() bool {
-	return false
+	return pb6.Get()
 }
 
 func checkPowerStatusL() bool {
-	return false
+	return pb7.Get()
 }
 
 func checkHeat() float32 {
 	return -1.2
+}
+
+func blinkLED() {
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	for {
+		led.Low()
+		time.Sleep(time.Millisecond * 1000)
+
+		led.High()
+		time.Sleep(time.Millisecond * 1000)
+	}
 }
