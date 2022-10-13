0bf1ba8eabb920fcaabc37733220a4dbd2387f76
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-10-13 10:38:11 +0200

    Update Sensor.go

--- Sensor/Sensor.go
+++ Sensor/Sensor.go
@@ -15,19 +15,33 @@ func main() {
 	hV := machine.ADC{machine.ADC2}
 	ldr.Configure(machine.ADCConfig{})
 	for {
-		led.Set(uint16(3000))
-		lightSensor(ldr)
+		A := lightSensor(ldr)
+		changeLight(A, led)
 		highVoltageSensor(hV)
 		time.Sleep(time.Second)
 	}
 }
 
-func lightSensor(ldr machine.ADC) {
+func lightSensor(ldr machine.ADC) float32 {
 	fl := float32(ldr.Get())
 	println((fl / 65535.0) * 5.0)
+	return (fl / 65535.0) * 5.0
 }
 
-func highVoltageSensor(hV machine.ADC) {
+func highVoltageSensor(hV machine.ADC) float32 {
 	fl := float32(hV.Get())
 	println((fl / 65535.0) * 5.0)
+	return (fl / 65535.0) * 5.0
+}
+
+func changeLight(inLight float32, led machine.PWM) {
+	if inLight > 2.7 {
+		led.Set(uint16(0))
+	} else if inLight > 2.9 {
+		led.Set(uint16(12000))
+	} else if inLight > 3.2 && inLight < 4 {
+		led.Set(uint16(42000))
+	} else {
+		led.Set(uint(3000))
+	}
 }
