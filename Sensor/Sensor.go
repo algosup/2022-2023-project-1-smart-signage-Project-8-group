7ffecdc8b5be2630790abf11a0b6d73a6fb95302
Polonit <71769482+Louis-de-Lavenne-de-Choulot@users.noreply.github.com>
2022-10-21 20:36:16 +0200

    update receiver and sending

--- Sensor/Sensor.go
+++ Sensor/Sensor.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"machine"
-	"time"
 )
 
 var (
-	lSV         float32 // The value of the light sensor
-	highVoltage float32 // The value of the high voltage sensor
-	lowVoltage  float32 // The value of the low voltage sensor
+	switchFunc    bool    = true
+	lSV           float32       // The value of the light sensor
+	highVoltage   float32       // The value of the high voltage sensor
+	lowVoltage    float32       // The value of the low voltage sensor
+	waitTime      int8    = 15  // The time to wait before sending the next message
+	maxBrightness int8    = 100 // The maximum brightness of the LED
+	minBrightness int8    = 0   // The minimum brightness of the LED
+	stop          bool    = false
+	timeCounter   uint8   = 0
 )
 
 func main() {
@@ -26,25 +31,18 @@ func main() {
 
 	//main loop
 	for {
-		highVoltage = ADCSensor(hV) // Read the high voltage sensor
-		lowVoltage = ADCSensor(lV)  // Read the low voltage sensor
-
-		// get time of day, if night time between 01:00 and 06:00, set lights to 0%
-		// if time.Now().Hour() >= 1 && time.Now().Hour() <= 6 {
-		// 	led.Set(0) // Set the brightness of the LED to 0%
-		// } else {
-		lightSensorValue := changeLight(ADCSensor(lS))
-		led.Set(lightSensorValue) // Change the LED brightness based on the light sensor value
-		// }
-
-		//create percentage of light sensor value and return it in rune
-		str := string(int(float32(lightSensorValue) * 100 / 65535))
-
-		InitAT() //make sure the AT module is ready
-
-		SendMessage(str) //send the message to the gateway
-
-		time.Sleep(time.Second * 1) // Sleep for 15 minutes
+		if switchFunc && !stop {
+			println("begin")
+			mainProg(led, lS, hV, lV)
+			switchFunc = false
+		} else {
+			if stop {
+				led.Set(0)
+			}
+			println("begin 2")
+			ReadMessage(waitTime + 1)
+			switchFunc = true
+		}
 	}
 }
 
@@ -57,5 +55,43 @@ func ADCSensor(adc machine.ADC) float32 {
 // Handle the PWM LED and set the brightness based on the light sensor value
 func changeLight(inLight float32) uint16 {
 	//return percentage of inLight out of 65535
-	return uint16((100 - (inLight * 20)) * 65535 * 0.01)
+	val := uint16(((5 - inLight) * 20) * 65535 / 100)
+	if val > uint16(float32(65535)/100*float32(maxBrightness)) {
+		return uint16(float32(65535) / 100 * float32(maxBrightness))
+	} else if val < uint16(float32(65535)/100*float32(minBrightness)) {
+		return uint16(float32(65535) / 100 * float32(minBrightness))
+	}
+	return val
+}
+
+func mainProg(led machine.PWM, lS machine.ADC, hV machine.ADC, lV machine.ADC) {
+	lightSensorValue := changeLight(ADCSensor(lS)) // Get the value of the light sensor
+	led.Set(lightSensorValue)                      // Change the LED brightness based on the light sensor value
+
+	highVoltage = ADCSensor(hV) // Read the high voltage sensor
+	lowVoltage = ADCSensor(lV)  // Read the low voltage sensor
+
+	println("highVoltage: ", highVoltage)
+	InitAT() //make sure the AT module is ready
+
+	str := string(rune((uint16(float32(lightSensorValue) / 65535 * 100)))) //create percentage of light sensor value and return it in rune
+
+	if lightSensorValue != 0 {
+		timeCounter++
+		if timeCounter == 255 {
+			timeCounter = 0
+		}
+		str += string(rune(uint16(timeCounter)))
+	} else {
+		str += string(rune(uint16(0)))
+	}
+
+	//fake temporary data percentage added in str
+	str += string(rune(57))
+
+	//add 0x00 in string to the end of the string
+	str += "F"
+
+	println("str: ", str)
+	SendMessage(str) //send the message to the gateway
 }
